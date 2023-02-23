10369796017406fdddde7d59d66d99492a5fb561
Offeibea123 <dodoooffeibea@gmail.com>
2023-02-23 15:34:14 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+ * main -is the entry point of the programme and is needed in every C programe.
+ */
+#include <stdio.h>
+
+int main(void)
+{"Programming is like building a multilingual puzzle"
+	return (0);
+
+}
