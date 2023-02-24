7a25709a897eaa75f9a18fca8ffdc18bb9d0fcbd
Offeibea123 <dodoooffeibea@gmail.com>
2023-02-23 18:01:21 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main -is an entry point for every c programming
+ * main - is an entry point for every c programming
  *
- * Return Always 0 (Sucess)
+ * Return: Always 0 (Sucess)
  */
-int my_function(void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle/n");
 	return (0);
