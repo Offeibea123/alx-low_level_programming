1da5df8e10b98d849ab3f3cef3372055451e9d65
Offeibea123 <dodoooffeibea@gmail.com>
2023-02-23 17:23:49 -0800

    4-prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#inlude < stdio.h >
+#inlude <stdio.h>
 /**
  * main -is an entry point for every c programming
  * Return Always 0 (Sucess)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle /n");
 	return (0);
 }
