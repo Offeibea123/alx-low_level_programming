881fc61e8ee1613769850d82d276e089ca1122d6
Offeibea123 <dodoooffeibea@gmail.com>
2023-02-24 09:45:29 -0800

    6size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point for every c programme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d;
+	int a;
+	long int b;
+	float f;
+	long long int c;
+	printf("Size	of	char: %lu 1 byte(s)\n", (unsigned long)sizeof(char));
+        printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(int));
+        printf("Size of a long int: %lu 4 byte(s)\n", (unsigned long)sizeof(long int));
+        printf("Size of a long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(long long int));
+        printf("Size of a float: %lu 4 byte(s)\n", (unsigned long)sizeof(float));
+}
+	return (0);
