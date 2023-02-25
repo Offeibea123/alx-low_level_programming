d91186521525686c43974fb463ebfcc40ef2cc13
Offeibea123 <dodoooffeibea@gmail.com>
2023-02-24 16:25:03 -0800

    ok

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - entry point for every c programme
- *
+ * main -entry point for every c programme
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	long long int b;
 printtf("Size of char: %lu 1 byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of long int %lu 4 byte(s)\n", (unsigned long)sizeof(long int));
 printf("Size of float: %lu 4 bytes(s)\n", (unsigned long)sizeof(float));
-printf("Size of long long int:%lu8bytes\n"(unsigned long)sizeof(long long int));
+printf("Size of long long int: %lu 8bytes\n",(unsigned long)sizeof(long long int));
 return (0);
 }
