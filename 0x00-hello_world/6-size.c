3ded03b38ae07616577407f342117870c2675f11
Offeibea123 <dodoooffeibea@gmail.com>
2023-02-24 16:35:12 -0800

    pool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of char: %lu 1 byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of long int %lu 4 byte(s)\n", (unsigned long)sizeof(long int));
 printf("Size of float: %lu 4 byte(s)\n", (unsigned long)sizeof(float));
-printf("Size of long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of long long int: %lu 4 byte(s)\n", (unsigned long)sizeof(b));
 return (0);
 }
