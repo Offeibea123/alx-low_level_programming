4747407ec0828028c7556b2b7b5e93aa5cad260e
Offeibea123 <dodoooffeibea@gmail.com>
2023-02-24 16:30:34 -0800

    wow

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 printf("Size of char: %lu 1 byte(s)\n", (unsigned long)sizeof(char));
 printf("Size of an int: %lu 4 byte(s)\n", (unsigned long)sizeof(int));
 printf("Size of long int %lu 4 byte(s)\n", (unsigned long)sizeof(long int));
-printf("Size of float: %lu 4 bytes(s)\n", (unsigned long)sizeof(float));
-printf("Size of long long int: %lu 8bytes\n",(unsigned long)sizeof(long long int));
+printf("Size of float: %lu 4 byte(s)\n", (unsigned long)sizeof(float));
+printf("Size of long long int: %lu 8 byte(s)\n", (unsigned long)sizeof(b));
 return (0);
 }
