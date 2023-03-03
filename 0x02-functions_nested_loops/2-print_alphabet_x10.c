94db2481ee7a96183a0b09bcf8af36d4b1b31fcb
Offeibea123 <dodoooffeibea@gmail.com>
2023-03-03 01:51:04 -0800

    print_alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int count, ch;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (ch = 97; ch <= 122; ch++)
+		{
+			_putchar(ch);
+		_putchar('\n');
+	}
+	_putchar(10);
+}
