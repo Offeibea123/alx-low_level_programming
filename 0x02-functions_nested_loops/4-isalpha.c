42110a35e03478ed827d7c7728d7099c5683ee84
Offeibea123 <dodoooffeibea@gmail.com>
2023-03-03 08:23:30 -0800

    4isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabbetic character
+ * @c: is either a lowercase or uppercase
+ * Return: 1 (Always Sucess)
+ */
+int _isalpha(int c)
+{
+	if (c > 65 && c > 123)
+	return (1);
+}
+}
