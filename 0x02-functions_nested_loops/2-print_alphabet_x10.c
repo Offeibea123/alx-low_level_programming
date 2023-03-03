d70e454c70b20c2ee7dcbce9217bae0c71a802f9
Offeibea123 <dodoooffeibea@gmail.com>
2023-03-03 07:03:02 -0800

    alphabet in lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,11 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j:
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (i = 97; i <= 122; i++)
+		for (j = 97; j <= 122; j++)
 			_putchar(i);
 		_putchar('\n');
 	}
