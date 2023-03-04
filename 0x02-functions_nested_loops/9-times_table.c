b44247028ff4d1a2a5776952be32ee9d83c23821
Offeibea123 <dodoooffeibea@gmail.com>
2023-03-04 09:53:38 -0800

    9tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,18 +9,18 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
-	for (col = 0; col <= 9; col++)		
+	for (col = 0; col <= 9; col++)
 	{
 	n = row * col;
 	if (n >= 10)
 	{
 	c1 = (row * col) / 10;
-	c2 = (row * col) % 10;	
+	c2 = (row * col) % 10;
 	_putchar(',');
 	_putchar(' ');
 	_putchar('0' + c1);
 	_putchar('0' + c2);
-		} 
+	}
 	else
 	{
 		if (col != 0)
