b31759af750549d5e459775407825b12674ef2b2
Offeibea123 <dodoooffeibea@gmail.com>
2023-03-04 12:15:44 -0800

    11prints

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *print_to_98 - prints all ntatural numbers
+ *@w: number to represent natural numbers from 0 to 98
+ */
+void print_to_98(int w)
+{
+if (n > 98)
+{
+for (; n > 98; n--)
+{
+printf(" % d", n)
+}
+}
+else if (n < 98)
+{
+for (; n < 98; n++)
+{
+printf(" % d", n)
+}
+}
+printf(" % d", n);
+}
