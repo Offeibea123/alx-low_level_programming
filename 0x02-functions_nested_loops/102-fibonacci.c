9c9525339ef39c20c479f276977c7d86dc758ef0
Offeibea123 <dodoooffeibea@gmail.com>
2023-03-05 11:06:15 -0800

    102fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
 /**
- * main - program entry
- *
- * Return: Always returns 0
+*main - Prints natural numbers
+*
+*
+*Return: Always 0.
 */
-
 int main(void)
 {
-	fibonacci();
-	return (0);
-}
-
-/**
- * fibonacci - Prints the sum of preceeding 2 numbers upto 50
-*/
-void fibonacci(void)
+int a, b;
+for (a = 1; a < 1024; a++)
 {
-	int i = 1, upper = 50;
-	long min = 1, max = 2;
-
-	while (i <= upper)
-	{
-		if (i == 1)
-			printf("%ld", min);
-		else if (i == 2)
-			printf(", %ld", max);
-		else
-		{
-			max += min;
-			min = max - min;
-			printf(", %ld", max);
-		}
-		i++;
-	}
-	printf("\n");
+if ((a % 3) == 0 || (a % 5) == 0)
+b += a;
+}
+printf(" % d\n", b);
+return (0);
 }
