984c4e8b0211501f4fb6506aced7296d311f1a00
Offeibea123 <dodoooffeibea@gmail.com>
2023-03-07 09:48:41 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncat - a function that links two strings
+ *@dest: inputs for src
+ *@src: inputs for dest
+ *@n: bytes for src
+ *Return: 0 (Always success)
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+int count = 0, count2 = 0;
+
+while (*(dest + count) != '\0')
+{
+count++;
+}
+
+while (count2 < n)
+{
+*(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
+count++;
+count2++;
+}
+return (dest);
+}
+
